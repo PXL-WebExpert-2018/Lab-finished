11ed3e666f8c450b9808b8f88a9869f2917714c7
Niek <niek.vandael@alpinedigital.be>
2019-08-11 14:19:33 +0200

    initial commit

---
+++ src/app/models/contact.model.ts
@@ -0,0 +1,5 @@
+export class Contact {
+  constructor(private name: string, private email: string, private phone: string, private isFavorite = false, private avatar = 'assets/avatar.png') {
+  }
+}
+
